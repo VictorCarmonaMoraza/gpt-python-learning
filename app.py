1111f30a346f7be82f4c6d610ab96c5549b051bb
Victor Carmona <victorcarmona9035.com>
2025-10-04 18:35:59 +0200

    Obtener varios resultados invocando al chat de OpenAI con generate

--- app.py
+++ app.py
@@ -13,8 +13,25 @@ chat = ChatOpenAI(api_key=api_key, model="gpt-4o-mini")
 
 # caso 2
 # Especificamos la personalidad que debe tener el sistema
-resultado = chat.invoke([SystemMessage(content='Eres un historiador que conoce'
-                                               ' los detalles de todas las ciudades del mundo'),
-                         HumanMessage(content='¿Puedes decirme donde se encuentra caceres?')])
+# resultado = chat.invoke([SystemMessage(content='Eres un historiador que conoce'
+# ' los detalles de todas las ciudades del mundo'),
+# HumanMessage(content='¿Puedes decirme donde se encuentra caceres?')])
 
-print(resultado.content)
+# print(resultado.content)
+
+##caso 3
+# Obtener varios resultados invocando al chat de OpenAI con "generate"
+resultado = chat.generate([
+    [
+        SystemMessage(content='Eres un historiador que conoce los detalles de todas las ciudades del mundo'),
+        HumanMessage(content='¿Puedes decirme dónde se encuentra Cáceres?')
+    ],
+    [
+        SystemMessage(content='Eres un joven rudo que no le gusta que le pregunten, solo quiere estar de fiesta'),
+        HumanMessage(content='¿Puedes decirme dónde se encuentra Cáceres?')
+    ]
+])
+
+print(f'El primer resultado es {resultado.generations[0][0].text}')
+
+print(f'El segundo resultado es {resultado.generations[1][0].text}')
