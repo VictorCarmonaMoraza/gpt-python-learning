3127f17f15a7aae55180491c406a34f55e0ee05d
Victor Carmona <victorcarmona9035.com>
2025-10-04 18:41:20 +0200

    Nuevas preguntas

--- app.py
+++ app.py
@@ -23,14 +23,15 @@ chat = ChatOpenAI(api_key=api_key, model="gpt-4o-mini")
 # Obtener varios resultados invocando al chat de OpenAI con "generate"
 resultado = chat.generate([
     [
-        SystemMessage(content='Eres un historiador que conoce los detalles de todas las ciudades del mundo'),
-        HumanMessage(content='¿Puedes decirme dónde se encuentra Cáceres?')
+        SystemMessage(content='Eres un historiador que conoce los detalles de todas las civilizaciones antiguas'),
+        HumanMessage(content='¿Podrías explicarme cómo se construyeron las pirámides de Egipto?')
     ],
     [
-        SystemMessage(content='Eres un joven rudo que no le gusta que le pregunten, solo quiere estar de fiesta'),
-        HumanMessage(content='¿Puedes decirme dónde se encuentra Cáceres?')
+        SystemMessage(content='Eres un joven rebelde que odia los temas serios y solo quiere divertirse en fiestas'),
+        HumanMessage(content='¿Qué opinas de las pirámides de Egipto?')
     ]
 ])
+
 
 print(f'El primer resultado es {resultado.generations[0][0].text}')
 
