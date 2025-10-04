04994a7387979531b7be6446fdb8a8be5608adb7
Victor Carmona <victorcarmona9035.com>
2025-10-04 18:21:39 +0200

    Especificamos personalidad que debe tomar el sistema

--- app.py
+++ app.py
@@ -4,8 +4,17 @@ from langchain_openai import ChatOpenAI
 from langchain.schema import SystemMessage, HumanMessage
 
 f = open('api_key.txt')
-api_key =f.read()
-chat= ChatOpenAI(api_key =api_key,model="gpt-4o-mini")
+api_key = f.read()
+chat = ChatOpenAI(api_key=api_key, model="gpt-4o-mini")
 
-resultado = chat.invoke([HumanMessage(content="¿Puedes decirme la hora en sevilla?")])
+# caso 1
+# resultado = chat.invoke([HumanMessage(content="¿Puedes decirme la hora en sevilla?")])
+# print(resultado.content)
+
+# caso 2
+# Especificamos la personalidad que debe tener el sistema
+resultado = chat.invoke([SystemMessage(content='Eres un historiador que conoce'
+                                               ' los detalles de todas las ciudades del mundo'),
+                         HumanMessage(content='¿Puedes decirme donde se encuentra caceres?')])
+
 print(resultado.content)
